98024f7d842b24a240e96b9c5f85e9b8ffef03ef
Rafael Pernil <rafaelpernil@uma.es>
2019-12-22 18:39:05 +0100

    feat: Init test definition

---
+++ src/tests/promise-batch-test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import ko from 'knockout';
+import 'mocha';
+import { PromiseBatch } from '../index';
+import { PromiseBatchStatus } from '../index';
+import { IAnyObject } from '../interfaces/i-any-object';
+import { ICustomPromise } from '../interfaces/i-custom-promise';
+import { PromiseUtil } from '../utils/promise-util';
+
+describe('PromiseBatch', () => {
+  it('does something', async () => {
+    // Implement if needed
+  });
+});
